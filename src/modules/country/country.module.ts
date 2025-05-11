b777b80b5fb3b04d272fa632b1cbc090628a8520
Quentin Collet <q.hyponerso@gmail.com>
2025-05-11 15:07:23 +0200

    refactor: remove all files deleted

--- src/modules/country/country.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { CountryService } from "./country.service";
-
-@Module({
-	providers: [CountryService],
-})
-export class CountryModule {}
--- src/modules/participant/participant.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ParticipantService } from "./participant.service";
-
-@Module({
-	providers: [ParticipantService],
-})
-export class ParticipantModule {}
